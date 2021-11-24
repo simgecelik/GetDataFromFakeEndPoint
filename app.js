0badac075f4a63771498529753c674a09ade3548
simgecelik <simge_celik_98@hotmail.com>
2021-11-24 13:39:35 +0300

    first commit

--- app.js
+++ app.js
@@ -1,12 +1,8 @@
 import axios from "axios"
 const getData = async(number)=>{
-    const {data: users} =  await axios(`https://jsonplaceholder.typicode.com/users/${number}`);
-    
-    const {data: posts} =  await axios(`https://jsonplaceholder.typicode.com/posts?userId=${number}`)
-
-  
+    const {data: users} =  await axios("https://jsonplaceholder.typicode.com/users/${number}");
+    const {data: posts} =  await axios("https://jsonplaceholder.typicode.com/posts?userId=${number}")
     return {"users" : users, "posts" : posts}
 }
-
 
 export default getData;
